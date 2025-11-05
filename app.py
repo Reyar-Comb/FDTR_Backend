5e7d6c4bd608877f46f4a2a43ffbe7a81e42ba2e
Reyar <2290197845@qq.com>
2025-11-06 02:46:09 +0800

    Added simple tailwind table for leaderboard

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import database
 
 app = Flask(__name__)
+CORS(app)
 database.init_db()
 
 @app.route('/test', methods=['GET'])
